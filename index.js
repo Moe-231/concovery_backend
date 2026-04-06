fc7950007a1d3a76fe5d78a570bced9335df1664
Moe-231 <cmut0006@student.monash.edu>
2026-04-06 16:42:39 +1000

    Initial Backend Setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const cors = require('cors')
+const express = require('express')
+const dotenv = require('dotenv')
+const bodyParser = require("body-parser");
+const postGress = require("./routes/postGress")
+
+const app = express()
+
+dotenv.config()
+
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.use("/postgress", postGress)
+
+const PORT = 8080
+app.listen(PORT, () => {
+    `Server is running on port ${PORT}`
+})
